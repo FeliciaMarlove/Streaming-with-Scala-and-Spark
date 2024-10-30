348ff28f6ba5b4b76ca5b9d4d9b88fd6341b2c15
aivcp <aivcp@goiba.net>
2024-10-30 07:39:40 +0100

    Window, rolling average

--- src/main/scala/Detector.scala
+++ src/main/scala/Detector.scala
@@ -1,7 +1,6 @@
 package com.thecodeshewrites.frauddetector
 
-// dummy change!
-
+import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import org.apache.spark.sql.functions._
 
@@ -13,16 +12,32 @@ object Detector {
       .master("local")
       .appName("Fraud Detector")
       .config("spark.driver.memory", "2g")
+      .config("spark.sql.legacy.timeParserPolicy", "LEGACY") // or "CORRECTED" => fix dates parsing problem (probably a dirty fix)
       .enableHiveSupport
       .getOrCreate()
 
     import spark.implicits._
     val financesDF = spark.read.json("Data/finances-small.json")
+
+    // Windows => utilize data before or after or both
+    val accountNumberPrevious4WindowSpec = Window.partitionBy($"AccountNumber") // returns a WindowSpec object
+                                                    .orderBy($"Date")
+                                                    .rowsBetween(-4,0) // take the 4 rows before current
+                                                    //.rangeBetween(-4,0) // => instead of rowsBetween; based on the value, not the position (Long values)
+    // rolling avg aka moving avg = calculate a trend over a short period of time
+    val rollingAvgForPrevious4PerAccount = avg($"Amount").over(accountNumberPrevious4WindowSpec)
+
     financesDF
       .na.drop("all", Seq("ID","Account","Amount","Description","Date")) // drop rows were ALL columns (from the selection) are null or NaN
       .na.fill("Unknown", Seq("Description")) // replace null values selected columns
       .where($"Amount" =!= 0 || $"Description" === "Unknown")
-      .selectExpr("Account.Number as AccountNumber", "Amount", "Date", "Description")
+      .selectExpr("Account.Number as AccountNumber",
+                  "Amount",
+                  // written in sql*
+                  "to_date(CAST(unix_timestamp(Date, 'MM/dd/yyyy') AS TIMESTAMP)) AS Date",
+                  "Description"
+      )
+      .withColumn("RollingAverage", rollingAvgForPrevious4PerAccount)
       .write.mode(SaveMode.Overwrite).parquet("Output/finances-small")
 
     if(financesDF.hasColumn("_corrupt_record")) {
@@ -44,6 +59,7 @@ object Detector {
 
     financesDF
       .select($"Account.Number".as("AccountNumber"), $"Amount", $"Description",
+        // same as the SQL but written with functions*
         to_date(unix_timestamp($"Date","MM/dd/yyyy").cast("timestamp")).as("Date"))
       .groupBy($"AccountNumber")
       .agg(avg($"Amount").as("AverageTransaction"), sum($"Amount").as("TotalTransactions"),
